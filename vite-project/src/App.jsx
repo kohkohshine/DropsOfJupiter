9560b0f41f49622327e746c67e4eaf257e9177a6
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2023-09-26 20:01:42 +0200

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Cards from './assets/components/cards';
+import NavBar from './assets/components/navbar';
+import Footer from './assets/components/footer';
+
+function App() { 
+
+  return (
+    <>
+     <NavBar/>
+     <Cards/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
